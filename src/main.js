172355bddea15970f0fcd6ec5ea95759ad2652e6
Peter Dierx <peterdierx@gmail.com>
2019-06-09 18:54:25 +0200

    default cleanup

--- src/main.js
+++ src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+// MODULES
+import Vue    from 'vue'
+import App    from '@/App.vue'
+import router from '@/router'
+import store  from '@/store'
 
+// CONFIG
 Vue.config.productionTip = false
 
+// APP
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: h => h( App )
+}).$mount( '#app' )
