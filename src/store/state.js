4238b47314a44ad3bb419a6b9400efa459d6f952
Peter Dierx <peterdierx@gmail.com>
2019-06-10 15:45:17 +0200

    Things store setup

---
+++ src/store/state.js
@@ -0,0 +1,13 @@
+// STATE
+export default {
+
+  categories: [
+    { name: 'Inbox',     selected: true  },
+    { name: 'Today',     selected: false },
+    { name: 'Upcoming',  selected: false },
+    { name: 'Someday',   selected: false },
+    { name: 'Completed', selected: false },
+    { name: 'Archive',   selected: false }
+  ]
+  
+}
