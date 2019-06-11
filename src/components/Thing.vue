7887edf09cbc8d50023c51bf2528e649c6f49d33
Peter Dierx <peterdierx@gmail.com>
2019-06-11 14:31:19 +0200

    Thing component

---
+++ src/components/Thing.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class='field'>
+    <p class='control'>
+      <div class='input'>{{ titel }}</div>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Thing',
+    props: [ 'titel', 'id' ]
+  }
+</script>
